d09be384c5f9774a7130d480cc79fd6ac23949d5
Jeevan Thapa <jeevanthapa1987@yahoo.com>
2024-04-21 21:56:42 -0400

    create the user log in

--- controllers/index.js
+++ controllers/index.js
@@ -1,11 +1,9 @@
 const router = require('express').Router();
 
-const apiRoutes = require('./api');
-const routes = require('./controllers/');
-router.use('/api', apiRoutes);
+const apiRoutes = require('./api/');
+const homeRoutes = require('./home-routes.js');
 
-router.use((req, res) => {
-  res.status(404).end();
-});
+router.use('/', homeRoutes);
+router.use('/api', apiRoutes);
 
 module.exports = router;
